6edc0c5eb4ce111a71ff1a98f763796af2c5d76b
Prajwal B M <bmprajwal43@gmail.com>
2025-03-23 21:39:13 +0530

    header for home page is completed

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,19 @@
-import { create } from "zustand";
-import { produce } from "immer";
-import { persist, createJSONStorage } from "zustand/middleware";
-import { useAsyncStorage } from "@react-native-async-storage/async-storage";
-import BeansData from "../data/BeansData";
-import CoffeeData from "../data/CoffeeData";
+import { create } from 'zustand';
+import { produce } from 'immer';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import BeansData from '../data/BeansData';
+import CoffeeData from '../data/CoffeeData';
+
+export const useStore = create(
+    persist(
+        (set, get) => ({
+            CoffeeList: CoffeeData,
+            BeanList: BeansData,
+            CartPrice: 0,
+            FavoritesList: [],
+            CartList: [],
+            OrderHistoryList: [],
+        }), { name: 'coffee-app', storage: createJSONStorage(() => AsyncStorage) }
+    ),
+);
